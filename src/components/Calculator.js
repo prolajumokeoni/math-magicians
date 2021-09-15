493b5e3547ba84aee24ee7b783aee35a6f3ace71
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-15 09:45:59 +0100

    Add calculator class

---
+++ src/components/Calculator.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div>0</div>
+        <div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
