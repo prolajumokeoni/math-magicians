c60a93a214044f676d9492631fcaf539683e73ef
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-15 18:06:49 +0100

    Add events to input buttons and  Set states

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,45 +1,87 @@
 import React, { Component } from 'react';
+import calculate from '../logic/calculate';
+import Display from './Display';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: 0,
+      operation: '',
+      next: '',
+    };
   }
 
+  manageClicks = (e) => {
+    this.setState((obj) => calculate(obj, e.target.value));
+  };
+
   render() {
+    const { total, operation, next } = this.state;
     return (
       <div className="appCalc">
-        <div className="screenInput">0</div>
-        <div className="btnBox">
-          <input type="button" value="AC" />
-          <input type="button" value="+/-" />
-          <input type="button" value="%" />
-          <input type="button" className="orange" value="÷" />
+        <div className="screenInput">
+          <Display total={total} operation={operation} next={next} />
         </div>
         <div className="btnBox">
-          <input type="button" value="7" />
-          <input type="button" value="8" />
-          <input type="button" value="9" />
-          <input type="button" className="orange" value="X" />
+          <input type="button" onClick={this.manageClicks} value="AC" />
+          <input type="button" onClick={this.manageClicks} value="+/-" />
+          <input type="button" onClick={this.manageClicks} value="%" />
+          <input
+            type="button"
+            className="orange"
+            onClick={this.manageClicks}
+            value="÷"
+          />
         </div>
         <div className="btnBox">
-          <input type="button" value="4" />
-          <input type="button" value="5" />
-          <input type="button" value="6" />
-          <input type="button" className="orange" value="-" />
+          <input type="button" onClick={this.manageClicks} value="7" />
+          <input type="button" onClick={this.manageClicks} value="8" />
+          <input type="button" onClick={this.manageClicks} value="9" />
+          <input
+            type="button"
+            className="orange"
+            onClick={this.manageClicks}
+            value="X"
+          />
         </div>
         <div className="btnBox">
-          <input type="button" value="1" />
-          <input type="button" value="2" />
-          <input type="button" value="3" />
-          <input type="button" className="orange" value="+" />
+          <input type="button" onClick={this.manageClicks} value="4" />
+          <input type="button" onClick={this.manageClicks} value="5" />
+          <input type="button" onClick={this.manageClicks} value="6" />
+          <input
+            type="button"
+            className="orange"
+            onClick={this.manageClicks}
+            value="-"
+          />
         </div>
         <div className="btnBox">
-          <input type="button" className="zero" value="0" />
-          <input type="button" value="." />
-          <input type="button" className="orange" value="=" />
+          <input type="button" onClick={this.manageClicks} value="1" />
+          <input type="button" onClick={this.manageClicks} value="2" />
+          <input type="button" onClick={this.manageClicks} value="3" />
+          <input
+            type="button"
+            className="orange"
+            onClick={this.manageClicks}
+            value="+"
+          />
         </div>
-
+        <div className="btnBox">
+          <input
+            type="button"
+            className="zero"
+            onClick={this.manageClicks}
+            value="0"
+          />
+          <input type="button" onClick={this.manageClicks} value="." />
+          <input
+            type="button"
+            className="orange"
+            onClick={this.manageClicks}
+            value="="
+          />
+        </div>
       </div>
     );
   }
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 import Big from 'big.js';
 
 export default function operate(numberOne, numberTwo, operation) {
