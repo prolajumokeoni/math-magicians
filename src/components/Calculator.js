ccac661a72d45ad0bd58619f4a7dca4c2b13ed26
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-15 11:40:51 +0100

    Add buttons and styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -8,11 +8,38 @@ class Calculator extends Component {
 
   render() {
     return (
-      <div>
-        <div>0</div>
-        <div>
-          
+      <div className="appCalc">
+        <div className="screenInput">0</div>
+        <div className="btnBox">
+          <input type="button" value="AC" />
+          <input type="button" value="+/-" />
+          <input type="button" value="%" />
+          <input type="button" className="orange" value="÷" />
         </div>
+        <div className="btnBox">
+          <input type="button" value="7" />
+          <input type="button" value="8" />
+          <input type="button" value="9" />
+          <input type="button" className="orange" value="X" />
+        </div>
+        <div className="btnBox">
+          <input type="button" value="4" />
+          <input type="button" value="5" />
+          <input type="button" value="6" />
+          <input type="button" className="orange" value="-" />
+        </div>
+        <div className="btnBox">
+          <input type="button" value="1" />
+          <input type="button" value="2" />
+          <input type="button" value="3" />
+          <input type="button" className="orange" value="+" />
+        </div>
+        <div className="btnBox">
+          <input type="button" className="zero" value="0" />
+          <input type="button" value="." />
+          <input type="button" className="orange" value="=" />
+        </div>
+
       </div>
     );
   }
