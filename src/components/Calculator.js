cd5d583da018f9823c9d492a5a09b3b9938ddd6b
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-16 23:57:42 +0100

    Refactor calculator components

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,90 +1,80 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import calculate from '../logic/calculate';
 import Display from './Display';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: '',
-      next: '',
-    };
-  }
+const calculator = () => {
+  const [obj, setState] = useState({ total: null, next: null, operation: null });
 
-  manageClicks = (e) => {
-    this.setState((obj) => calculate(obj, e.target.value));
+  const manageClicks = (e) => {
+    setState(calculate(obj, e.target.value));
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="appCalc">
-        <div className="screenInput">
-          <Display total={total} operation={operation} next={next} />
-        </div>
-        <div className="btnBox">
-          <input type="button" onClick={this.manageClicks} value="AC" />
-          <input type="button" onClick={this.manageClicks} value="+/-" />
-          <input type="button" onClick={this.manageClicks} value="%" />
-          <input
-            type="button"
-            className="orange"
-            onClick={this.manageClicks}
-            value="÷"
-          />
-        </div>
-        <div className="btnBox">
-          <input type="button" onClick={this.manageClicks} value="7" />
-          <input type="button" onClick={this.manageClicks} value="8" />
-          <input type="button" onClick={this.manageClicks} value="9" />
-          <input
-            type="button"
-            className="orange"
-            onClick={this.manageClicks}
-            value="x"
-          />
-        </div>
-        <div className="btnBox">
-          <input type="button" onClick={this.manageClicks} value="4" />
-          <input type="button" onClick={this.manageClicks} value="5" />
-          <input type="button" onClick={this.manageClicks} value="6" />
-          <input
-            type="button"
-            className="orange"
-            onClick={this.manageClicks}
-            value="-"
-          />
-        </div>
-        <div className="btnBox">
-          <input type="button" onClick={this.manageClicks} value="1" />
-          <input type="button" onClick={this.manageClicks} value="2" />
-          <input type="button" onClick={this.manageClicks} value="3" />
-          <input
-            type="button"
-            className="orange"
-            onClick={this.manageClicks}
-            value="+"
-          />
-        </div>
-        <div className="btnBox">
-          <input
-            type="button"
-            className="zero"
-            onClick={this.manageClicks}
-            value="0"
-          />
-          <input type="button" onClick={this.manageClicks} value="." />
-          <input
-            type="button"
-            className="orange"
-            onClick={this.manageClicks}
-            value="="
-          />
-        </div>
+  return (
+    <div className="appCalc">
+      <div className="screenInput">
+        <Display total={obj.total} operation={obj.operation} next={obj.next} />
       </div>
-    );
-  }
-}
+      <div className="btnBox">
+        <input type="button" onClick={manageClicks} value="AC" />
+        <input type="button" onClick={manageClicks} value="+/-" />
+        <input type="button" onClick={manageClicks} value="%" />
+        <input
+          type="button"
+          className="orange"
+          onClick={manageClicks}
+          value="÷"
+        />
+      </div>
+      <div className="btnBox">
+        <input type="button" onClick={manageClicks} value="7" />
+        <input type="button" onClick={manageClicks} value="8" />
+        <input type="button" onClick={manageClicks} value="9" />
+        <input
+          type="button"
+          className="orange"
+          onClick={manageClicks}
+          value="x"
+        />
+      </div>
+      <div className="btnBox">
+        <input type="button" onClick={manageClicks} value="4" />
+        <input type="button" onClick={manageClicks} value="5" />
+        <input type="button" onClick={manageClicks} value="6" />
+        <input
+          type="button"
+          className="orange"
+          onClick={manageClicks}
+          value="-"
+        />
+      </div>
+      <div className="btnBox">
+        <input type="button" onClick={manageClicks} value="1" />
+        <input type="button" onClick={manageClicks} value="2" />
+        <input type="button" onClick={manageClicks} value="3" />
+        <input
+          type="button"
+          className="orange"
+          onClick={manageClicks}
+          value="+"
+        />
+      </div>
+      <div className="btnBox">
+        <input
+          type="button"
+          className="zero"
+          onClick={manageClicks}
+          value="0"
+        />
+        <input type="button" onClick={manageClicks} value="." />
+        <input
+          type="button"
+          className="orange"
+          onClick={manageClicks}
+          value="="
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Calculator;
+export default calculator;
