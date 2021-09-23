743b550569ba006b413fc2fbc39a56c3ea8011ef
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-23 11:10:27 +0100

    Add content for web page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div className="d-flex justify-content-between">
-    <ul className="nav">
-      <li className="nav-item">
-        <Link to="/" className="nav-link">Math Magicians</Link>
-      </li>
-    </ul>
-    <ul className="nav">
-      <li className="nav-item">
-        <Link to="/" className="nav-link">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/calculator" className="nav-link">Calculator</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/about" className="nav-link">About</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/Quotes" className="nav-link">Quotes</Link>
-      </li>
-    </ul>
-  </div>
+  <section>
+    <div className="d-flex justify-content-between pt-3">
+      <ul className="nav">
+        <li className="nav-item">
+          <Link to="/about" className="nav-link link-dark fs-4 fw-bolder">Math Magicians</Link>
+        </li>
+      </ul>
+      <ul className="nav">
+        <li className="nav-item">
+          <u><Link to="/about" className="nav-link border-end">Home</Link></u>
+        </li>
+        <li className="nav-item">
+          <u><Link to="/calculator" className="nav-link border-end">Calculator</Link></u>
+        </li>
+        <li className="nav-item">
+          <u><Link to="/Quotes" className="nav-link border-end">Quotes</Link></u>
+        </li>
+      </ul>
+    </div>
+  </section>
 );
 
 export default Navbar;
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 const Quotes = () => (
-  <div />
+  <div className="container">
+    <p className="fs-5 p-5 pb-0 m-5 mb-0 text-center">Our lives are not determined by what happens to us but how we react to what happens, not by what life brings us but the attitude we bring to life.</p>
+    <p className="text-center">Wade Boggs</p>
+  </div>
 );
 
 export default Quotes;
