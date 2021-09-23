6da41feab00ba9d8da7be6a11406fd527c13fb6d
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-23 06:05:25 +0100

    Import files and add to router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
+import Quotes from './components/Quotes';
+import About from './components/About';
 import './App.css';
 
-const App = () => (
-  <div className="app">
-    <Calculator />
-  </div>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/calculator">
+          <Calculator />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/quotes">
+          <Quotes />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
