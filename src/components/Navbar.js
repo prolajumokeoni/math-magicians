464494ba1897cbaaf3fec57f681d3d53c7753f21
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2021-09-23 09:37:49 +0100

    Add bootstrap and nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
+  <div className="d-flex justify-content-between">
+    <ul className="nav">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Math Magicians</Link>
       </li>
-      <li>
-        <Link to="/about">About</Link>
+    </ul>
+    <ul className="nav">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">Home</Link>
       </li>
-      <li>
-        <Link to="/Quotes">Quotes</Link>
+      <li className="nav-item">
+        <Link to="/calculator" className="nav-link">Calculator</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/about" className="nav-link">About</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/Quotes" className="nav-link">Quotes</Link>
       </li>
     </ul>
   </div>
